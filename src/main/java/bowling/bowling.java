fce917012a0518038c9b441f99619b22818d55c5
zhihua.su@goldenequator.com <zhihua.su@goldenequator.com>
2016-09-08 17:08:13 +0800

    bowling

---
+++ src/main/java/bowling/bowling.java
@@ -0,0 +1,33 @@
+package bowling;
+
+/**
+ * Created by zhihua on 9/8/16.
+ */
+public class bowling {
+    public static void main(String[] args) {
+        int[][] inData = {{1, 9}, {2, 7}, {10}, {10}, {10}, {0, 0}, {2, 7}, {10, 0}, {3, 7}, {10, 3, 3}};
+        int[] result = calculateScore(inData);
+        for (int r: result) {
+            System.out.println(r);
+        }
+    }
+
+    public static int[] calculateScore(int[][] inData) {
+        int len = inData.length;
+        int[] scores = new int[len];
+        for (int i=len-1; i>=0; i--) {
+            int[] frame = inData[i];
+            for (int score : frame) {
+                scores[i] += score;
+            }
+            if (i != len-1 && frame[0] == 10) {
+                if (scores[i+1] > 20) {
+                    scores[i] = 30;
+                } else {
+                    scores[i] += scores[i + 1];
+                }
+            }
+        }
+        return scores;
+    }
+}
