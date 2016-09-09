65dcd36dd91832fc19ee7cd5a692d6825c29dc0b
zhihua.su@goldenequator.com <zhihua.su@goldenequator.com>
2016-09-09 15:56:13 +0800

    changes

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -4,16 +4,63 @@ package bowling;
  * Created by zhihua on 9/8/16.
  */
 public class Bowling {
+    /**
+     * Calculate the score of the bowling session
+     * Null will be returned if the input is invalid
+     */
     public static int[] calculateScore(int[][] inData) {
+        if (inData == null) {
+            return null;
+        }
         int len = inData.length;
+        if (len != 10) {
+            return null;
+        }
         int[] scores = new int[len];
         for (int i=len-1; i>=0; i--) {
             int[] frame = inData[i];
+            //Check for valid array
+            if (frame == null || frame.length == 0) {
+                return null;
+            }
+            //Last frame
+            if (i==len-1) {
+                if (frame.length > 3) {
+                    return null;
+                }
+                if (frame[0] != 10 && frame.length > 2) {
+                    return null;
+                }
+            } else {
+                if (frame.length > 2) {
+                    return null;
+                }
+            }
+            //End check for valid array
             for (int score : frame) {
+                if (score > 10 || score < 0) {
+                    //Invalid individual score
+                    return null;
+                }
                 scores[i] += score;
             }
+            //Check for invalid score for every frame
+            if (i==len-1 && frame[0]==10) {
+                if (scores[i] > 20) {
+                    return null;
+                }
+            } else {
+                if (scores[i] > 10) {
+                    return null;
+                }
+            }
+            //End check for invalid score for every frame
+
+            //Main calculation
             if (i != len-1 && frame[0] == 10) {
-                if (scores[i+1] > 20) {
+                if (i <= len-4 && scores[i + 1] > 20) {
+                    //More than two continuous strikes
+                    //And before the 8th frame
                     scores[i] = 30;
                 } else {
                     scores[i] += scores[i + 1];
--- src/test/java/bowling/TestJunit.java
+++ src/test/java/bowling/TestJunit.java
@@ -12,8 +12,100 @@ public class TestJunit {
 
     @Test
     public void testCalculateScore() {
-        int[][] inData = {{1, 9}, {0, 10}, {10}, {10}, {10}, {0, 0}, {2, 7}, {10, 0}, {3, 7}, {10, 3, 3}};
-        int[] result = {10, 10, 30, 20, 10, 0, 9, 20, 10, 16};
+        assertArrayEquals(null, calculateScore(null));
+
+        int[][] inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, null, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2, 3, 4}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2, 3}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2, 3}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {0, 11}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {13, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {-2, 3}, {1, 2}, {1, 2}, {10, 2, 3}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {7, 8}, {1, 2}, {10, 2, 3}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {3, 8}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 8, 5}};
+        assertArrayEquals(null, calculateScore(inData));
+
+        int[] result;
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
+        result = new int[]{3, 3, 3, 3, 3, 3, 3, 3, 3, 15};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {0, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 3, 3, 3, 3, 2, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 0}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 3, 3, 3, 3, 1, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {10, 0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 13, 3, 3, 3, 3, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 23, 13, 3, 3, 3, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 30, 23, 13, 3, 3, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 30, 30, 23, 13, 3, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {0, 0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 20, 10, 0, 3, 3, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {10}, {0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+        result = new int[]{3, 3, 10, 0, 3, 3, 3, 3, 3, 3};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10, 2, 6}};
+        result = new int[]{3, 3, 3, 3, 3, 3, 3, 3, 28, 18};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10}, {10, 2, 6}};
+        result = new int[]{3, 3, 3, 3, 3, 3, 3, 38, 28, 18};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10}, {10}, {10, 2, 6}};
+        result = new int[]{3, 3, 3, 3, 3, 3, 30, 38, 28, 18};
+        assertArrayEquals(result, calculateScore(inData));
+
+        inData = new int[][]{{10}, {10}, {10}, {10}, {10}, {10}, {10}, {10}, {10}, {10, 10}};
+        result = new int[]{30, 30, 30, 30, 30, 30, 30, 40, 30, 20};
         assertArrayEquals(result, calculateScore(inData));
     }
 }
