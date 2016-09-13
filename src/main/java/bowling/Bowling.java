06f1c2d992529992057a99778dafdab46581defe
scarecrow2003 <scarecrow2003@gmail.com>
2016-09-13 23:21:16 +0800

    changes

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -4,6 +4,87 @@ package bowling;
  * Created by zhihua on 9/8/16.
  */
 public class Bowling {
+
+	private static int FRAMES_PER_GAME = 10;
+
+	private int[][] rawScore = null;
+
+	private int[] finalScore = null;
+
+	public void startGame() {
+		rawScore = new int[10][];
+		finalScore = new int[10];
+	}
+
+	public void restartGame() {
+		startGame();
+	}
+
+	/**
+	 * Obtain the raw score data
+	 * Return false if the supplied raw data is invalid
+	 */
+	public boolean obtainRawScore(int[][] inData) {
+		boolean dataValid = validateData(inData);
+		if (dataValid) {
+			rawScore = inData;
+		}
+		return dataValid;
+	}
+
+	private boolean validateData(int[][] inData) {
+		if (inData == null) {
+			return false;
+		}
+		int len = inData.length;
+		if (len != FRAMES_PER_GAME) {
+			return false;
+		}
+		for (int i=0; i<len; i++) {
+			int[] frame = inData[i];
+			boolean frameValid = validateFrame(frame, i);
+			if (!frameValid) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean validateFrame(int[] frame, int i) {
+		if (frame == null || frame.length == 0) {
+			return false;
+		}
+		if (i == FRAMES_PER_GAME-1) {
+			if (frame.length > 3) {
+				return false;
+			}
+			if (frame[0] != 10 && frame.length > 2) {
+				return false;
+			}
+		} else if (frame.length > 2) {
+			return false;
+		}
+		for (int score: frame) {
+			if (score > 10 || score < 0) {
+				return false;
+			}
+		}
+		if (i==len-1 && frame[0]==10) {
+                	if (frame[0] + frame[1]==null?0:frame[1] + frame[2]==null?0:frame[2] > 20) {
+                    		return false;
+                	}
+            	} else {
+                	if (frame[0] + frame[1]==null?0:frame[1] > 10) {
+                    		return false;
+                	}
+            	}
+		return true;
+	}
+
+	public void calculateScore() {
+
+	}
+
     /**
      * Calculate the score of the bowling session
      * Null will be returned if the input is invalid
