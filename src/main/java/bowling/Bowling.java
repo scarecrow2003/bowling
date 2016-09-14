52a6ef454995a331dc3022993f1a4993ac0f3a37
zhihua.su@goldenequator.com <zhihua.su@goldenequator.com>
2016-09-14 19:25:56 +0800

    changes

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -5,15 +5,101 @@ package bowling;
  */
 public class Bowling {
 
-	private static int FRAMES_PER_GAME = 10;
+	private static final int FRAMES_PER_GAME = 10;
+
+    private static final int MAX_SCORE_PER_HIT = 10;
 
 	private int[][] rawScore = null;
 
 	private int[] finalScore = null;
 
+    private boolean gameCompleted = false;
+
+    private boolean validateData(int[][] inData) {
+        if (inData == null) {
+            return false;
+        }
+        if (inData.length != FRAMES_PER_GAME) {
+            return false;
+        }
+        for (int i=0; i<FRAMES_PER_GAME; i++) {
+            int[] frame = inData[i];
+            boolean frameValid = validateFrame(frame, i);
+            if (!frameValid) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateFrame(int[] frame, int i) {
+        if (frame == null || frame.length == 0) {
+            return false;
+        }
+        if (i == FRAMES_PER_GAME-1) {
+            if (frame.length > 3) {
+                return false;
+            }
+            if (frame[0] != FRAMES_PER_GAME && frame.length > 2) {
+                return false;
+            }
+        } else if (frame.length > 2) {
+            return false;
+        }
+        for (int score: frame) {
+            if (score > 10 || score < 0) {
+                return false;
+            }
+        }
+        return validateFrameTotalScore(frame, i);
+    }
+
+    private boolean validateFrameTotalScore(int[] frame, int i) {
+        int total = 0;
+        for (int score: frame) {
+            total += score;
+        }
+        if (i==FRAMES_PER_GAME-1 && frame[0]==MAX_SCORE_PER_HIT) {
+            if (total > 20) {
+                return false;
+            }
+        } else {
+            if (total > 10) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Calculate the score of the bowling session
+     */
+    private void calculateScore() {
+        for (int i=FRAMES_PER_GAME-1; i>=0; i--) {
+            int[] frame = rawScore[i];
+            for (int score: frame) {
+                finalScore[i] += score;
+            }
+            if (i != FRAMES_PER_GAME-1 && frame[0] == MAX_SCORE_PER_HIT) {
+                if (i > FRAMES_PER_GAME-4) {
+                    finalScore[i] += finalScore[i+1];
+                } else {
+                    if (finalScore[i+1] > 20) {
+                        finalScore[i] = 30;
+                    } else {
+                        finalScore[i] += finalScore[i+1];
+                    }
+                }
+            }
+        }
+        for (int i=1; i<FRAMES_PER_GAME; i++) {
+            finalScore[i] += finalScore[i-1];
+        }
+    }
+
 	public void startGame() {
-		rawScore = new int[10][];
-		finalScore = new int[10];
+		rawScore = new int[FRAMES_PER_GAME][];
+        gameCompleted = false;
 	}
 
 	public void restartGame() {
@@ -28,130 +114,18 @@ public class Bowling {
 		boolean dataValid = validateData(inData);
 		if (dataValid) {
 			rawScore = inData;
+            gameCompleted = true;
 		}
 		return dataValid;
 	}
 
-	private boolean validateData(int[][] inData) {
-		if (inData == null) {
-			return false;
-		}
-		int len = inData.length;
-		if (len != FRAMES_PER_GAME) {
-			return false;
-		}
-		for (int i=0; i<len; i++) {
-			int[] frame = inData[i];
-			boolean frameValid = validateFrame(frame, i);
-			if (!frameValid) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public int[] getScore() {
+        finalScore = new int[FRAMES_PER_GAME];
+        calculateScore();
+        return finalScore;
+    }
 
-	private boolean validateFrame(int[] frame, int i) {
-		if (frame == null || frame.length == 0) {
-			return false;
-		}
-		if (i == FRAMES_PER_GAME-1) {
-			if (frame.length > 3) {
-				return false;
-			}
-			if (frame[0] != 10 && frame.length > 2) {
-				return false;
-			}
-		} else if (frame.length > 2) {
-			return false;
-		}
-		for (int score: frame) {
-			if (score > 10 || score < 0) {
-				return false;
-			}
-		}
-		if (i==len-1 && frame[0]==10) {
-                	if (frame[0] + frame[1]==null?0:frame[1] + frame[2]==null?0:frame[2] > 20) {
-                    		return false;
-                	}
-            	} else {
-                	if (frame[0] + frame[1]==null?0:frame[1] > 10) {
-                    		return false;
-                	}
-            	}
-		return true;
-	}
-
-	public void calculateScore() {
-
-	}
-
-    /**
-     * Calculate the score of the bowling session
-     * Null will be returned if the input is invalid
-     */
-    public static int[] calculateScore(int[][] inData) {
-        if (inData == null) {
-            return null;
-        }
-        int len = inData.length;
-        if (len != 10) {
-            return null;
-        }
-        int[] scores = new int[len];
-        for (int i=len-1; i>=0; i--) {
-            int[] frame = inData[i];
-            //Check for valid array
-            if (frame == null || frame.length == 0) {
-                return null;
-            }
-            //Last frame
-            if (i==len-1) {
-                if (frame.length > 3) {
-                    return null;
-                }
-                if (frame[0] != 10 && frame.length > 2) {
-                    return null;
-                }
-            } else {
-                if (frame.length > 2) {
-                    return null;
-                }
-            }
-            //End check for valid array
-            for (int score : frame) {
-                if (score > 10 || score < 0) {
-                    //Invalid individual score
-                    return null;
-                }
-                scores[i] += score;
-            }
-            //Check for invalid score for every frame
-            if (i==len-1 && frame[0]==10) {
-                if (scores[i] > 20) {
-                    return null;
-                }
-            } else {
-                if (scores[i] > 10) {
-                    return null;
-                }
-            }
-            //End check for invalid score for every frame
-
-            //Get the score for every frame
-            if (i != len-1 && frame[0] == 10) {
-                if (i <= len-4 && scores[i + 1] > 20) {
-                    //More than two continuous strikes
-                    //And before the 8th frame
-                    scores[i] = 30;
-                } else {
-                    scores[i] += scores[i + 1];
-                }
-            }
-        }
-        //Get the final result
-        for (int i=1; i<len; i++) {
-            scores[i] += scores[i-1];
-        }
-        return scores;
+	public boolean isGameCompleted() {
+        return gameCompleted;
     }
 }
--- src/test/java/bowling/TestJunit.java
+++ src/test/java/bowling/TestJunit.java
@@ -2,8 +2,9 @@ package bowling;
 
 import org.junit.Test;
 
-import static bowling.Bowling.calculateScore;
-import static org.junit.Assert.assertArrayEquals;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by zhihua on 9/8/16.
@@ -11,105 +12,159 @@ import static org.junit.Assert.assertArrayEquals;
 public class TestJunit {
 
     @Test
-    public void testCalculateScore() {
-        assertArrayEquals(null, calculateScore(null));
+    public void testValidateRawScore() {
+        Bowling game = new Bowling();
+        game.startGame();
+        assertFalse(game.obtainRawScore(null));
 
-        int[][] inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
 
-        inData = new int[][]{{1, 2}, null, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, null, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2, 3, 4}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2, 3, 4}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2, 3}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2, 3}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2, 3}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2, 3}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {0, 11}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {0, 11}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {13, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {13, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {-2, 3}, {1, 2}, {1, 2}, {10, 2, 3}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {-2, 3}, {1, 2}, {1, 2}, {10, 2, 3}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {7, 8}, {1, 2}, {10, 2, 3}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {7, 8}, {1, 2}, {10, 2, 3}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {3, 8}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {3, 8}}));
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 8, 5}};
-        assertArrayEquals(null, calculateScore(inData));
+        game.restartGame();
+        assertFalse(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 8, 5}}));
+    }
 
-        int[] result;
+    @Test
+    public void testCalculation() {
+        Bowling game = new Bowling();
+        game.startGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 9, 12, 15, 18, 21, 24, 27, 30}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}}));
+        assertArrayEquals(new int[]{3, 6, 9, 12, 15, 18, 21, 24, 27, 42}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10, 2, 3}};
-        result = new int[]{3, 6, 9, 12, 15, 18, 21, 24, 27, 42};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {0, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 9, 12, 15, 18, 20, 23, 26, 29}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {0, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 9, 12, 15, 18, 20, 23, 26, 29};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 0}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 9, 12, 15, 18, 19, 22, 25, 28}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 0}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 9, 12, 15, 18, 19, 22, 25, 28};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {10, 0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 19, 22, 25, 28, 31, 34, 37, 40}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {10, 0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 19, 22, 25, 28, 31, 34, 37, 40};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 29, 42, 45, 48, 51, 54, 57, 60}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 29, 42, 45, 48, 51, 54, 57, 60};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {10}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 36, 59, 72, 75, 78, 81, 84, 87}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 36, 59, 72, 75, 78, 81, 84, 87};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {10}, {10}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 36, 66, 89, 102, 105, 108, 111, 114}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {10}, {10}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 36, 66, 89, 102, 105, 108, 111, 114};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {10}, {10}, {0, 0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 26, 36, 36, 39, 42, 45, 48, 51}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {10}, {10}, {0, 0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 26, 36, 36, 39, 42, 45, 48, 51};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {10}, {0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}}));
+        assertArrayEquals(new int[]{3, 6, 16, 16, 19, 22, 25, 28, 31, 34}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {10}, {0}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-        result = new int[]{3, 6, 16, 16, 19, 22, 25, 28, 31, 34};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10, 2, 6}}));
+        assertArrayEquals(new int[]{3, 6, 9, 12, 15, 18, 21, 24, 52, 70}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10, 2, 6}};
-        result = new int[]{3, 6, 9, 12, 15, 18, 21, 24, 52, 70};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10}, {10, 2, 6}}));
+        assertArrayEquals(new int[]{3, 6, 9, 12, 15, 18, 21, 59, 87, 105}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10}, {10, 2, 6}};
-        result = new int[]{3, 6, 9, 12, 15, 18, 21, 59, 87, 105};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10}, {10}, {10, 2, 6}}));
+        assertArrayEquals(new int[]{3, 6, 9, 12, 15, 18, 48, 86, 114, 132}, game.getScore());
 
-        inData = new int[][]{{1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {10}, {10}, {10}, {10, 2, 6}};
-        result = new int[]{3, 6, 9, 12, 15, 18, 48, 86, 114, 132};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{10}, {10}, {10}, {10}, {10}, {10}, {10}, {10}, {10}, {10, 10}}));
+        assertArrayEquals(new int[]{30, 60, 90, 120, 150, 180, 210, 250, 280, 300}, game.getScore());
 
-        inData = new int[][]{{10}, {10}, {10}, {10}, {10}, {10}, {10}, {10}, {10}, {10, 10}};
-        result = new int[]{30, 60, 90, 120, 150, 180, 210, 250, 280, 300};
-        assertArrayEquals(result, calculateScore(inData));
+        game.restartGame();
+        assertTrue(game.obtainRawScore(new int[][]{{5,2},{8,1},{6,4},{10},{0,5},{2,6},{8,1},{5,3},{6,1},{10,2,6}}));
+        assertArrayEquals(new int[]{7,16,26,41,46,54,63,71,78,96}, game.getScore());
+    }
 
-        inData = new int[][]{{5,2},{8,1},{6,4},{10},{0,5},{2,6},{8,1},{5,3},{6,1},{10,2,6}};
-        result = new int[]{7,16,26,41,46,54,63,71,78,96};
-        assertArrayEquals(result, calculateScore(inData));
+    @Test
+    public void testValidateFrame() {
+        try {
+            Object game = Class.forName("Bowling").newInstance();
+            Method method = game.getClass().getDeclaredMethod("validateFrame", new Class<?>[0]);
+            assertFalse((Boolean) method.invoke(game, null, 0));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{}, 0));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{1, 1, 1, 4}, 9));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{1, 2, 3}, 9));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{1, 2, 3}, 0));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{12, 7}, 0));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{5, -7}, 0));
+
+            assertTrue((Boolean) method.invoke(game, new int[]{5, 3}, 0));
+
+            assertTrue((Boolean) method.invoke(game, new int[]{10, 3}, 9));
+
+            assertTrue((Boolean) method.invoke(game, new int[]{10, 3, 3}, 9));
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void testValidateFrameTotalScore() {
+        try {
+            Object game = Class.forName("Bowling").newInstance();
+            Method method = game.getClass().getDeclaredMethod("validateFrameTotalScore", new Class<?>[0]);
+            assertFalse((Boolean) method.invoke(game, new int[]{10, 7, 5}, 9));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{8, 8, 7}, 9));
+
+            assertFalse((Boolean) method.invoke(game, new int[]{5, 7}, 0));
+
+            assertTrue((Boolean) method.invoke(game, new int[]{5, 3}, 0));
+
+            assertTrue((Boolean) method.invoke(game, new int[]{10, 3}, 9));
+
+            assertTrue((Boolean) method.invoke(game, new int[]{10, 3, 3}, 9));
+        } catch (Exception e) {
+
+        }
     }
 }
